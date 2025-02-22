0c92a5338acc62807ef01509f8a042db70bcd9e0
Techical7 <chuayunjie.22@gmail.com>
2025-02-22 06:03:54 +0000

    Pending changes exported from your codespace

--- CT07_04/lesson4.py
+++ CT07_04/lesson4.py
@@ -14,13 +14,13 @@
 
 # planets = ["Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Nuptune" ]
 
-# # # Task 1b
+# Task 1b
 # planets[3] = "Watch"
 
 # for p in planets:
 #     print(p)
 
-# # Task 1c
+# Task 1c
 
 # planets.append("Pluto")
 # planets.insert(3,"lalaland")
@@ -32,7 +32,7 @@
 # for p in planets:
 #     print(p)
 
-# # Task 2
+# Task 2
 # for p in planets:
 
 #   if (p == "Earth"):
@@ -45,19 +45,38 @@
 #     print(p)
 
 # Task 3
-countries = []
+# countries = []
 
-user_inputs = ["Germany", "Japan", "end"]
-i = 0
+# user_inputs = ["Germany", "Japan", "end"]
+# i = 0
 
-while True:
-    country = user_inputs[i]
-    i += 1
+# while True:
+#     country = user_inputs[i]
+#     i += 1
 
-    if(country.lower() == "end"):
-        break
+#     if(country.lower() == "end"):
+#         break
 
-    countries.append(country)
+#     countries.append(country)
 
-for country in countries:
-    print(f"I would like to visit [country]")
+# for country in countries:
+#     print(f"I would like to visit [country]")
+
+# Task 4
+# list=[]
+
+# food = ["Apple", "Banana", "Cherries", "Dates", "Eggfruit", "Fazli", "Grape", "Hackberry", "Icaco", "Jackfruit", "Kiwi", "Lime"]
+
+# while True:
+#     user_input= input("What would you like to add into the menu")
+
+#     if(user_input.lower() == "end"):
+#         break
+
+#     food.append(user_input)
+
+# user_input= input("What would you like to eat")
+# if(user_input in food):
+#     print("Please come in")
+# else:
+#     print("Please go next door")
