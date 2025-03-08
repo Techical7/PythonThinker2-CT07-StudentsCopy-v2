829e81b63a181777795ffe9ca118618430fc5b84
Techical7 <chuayunjie.22@gmail.com>
2025-03-08 06:09:36 +0000

    Mar 8, 2025, 6:09 AM

--- CT07_07/lesson7.py
+++ CT07_07/lesson7.py
@@ -5,4 +5,4 @@ Health = 100
 print("Hero starts on his adventure with Health: 100")
 random.randint(1,15)
 if Health < 1:
-    print("After fighting monsters, his Health is now: ")
+    print("After fighting monsters, his Health is now:" + Health )
