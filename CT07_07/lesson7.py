2a78adc870ad8125b7daf17082e65f6bd9ddd49e
Techical7 <chuayunjie.22@gmail.com>
2025-03-08 06:43:47 +0000

    Mar 8, 2025, 6:43 AM

--- CT07_07/lesson7.py
+++ CT07_07/lesson7.py
@@ -1,17 +1,18 @@
-# import random
-# counter = []
-# Health = 100
-
-# print("Hero starts on his adventure with Health: 100")
-
-# while counter < 100:
-#      random_number = random.randint(1,15)
-#      Health -= random_number
-#      counter+=1
-
-# if Health < 1:
-#     print("After fighting monsters, his Health is now:" + Health "He fought" + counter "battles, and died.")
-#     print("After fighting monsters, his Health is now:" + Health)
+# List
+import random
+counter = []
+Health = 100
+counter = str
+print("Hero starts on his adventure with Health: 100")
+# Random health lost
+while counter < 100:
+     random_number = random.randint(1,15)
+     Health -= random_number
+     counter+=1
+# End
+if Health < 1:
+    print("After fighting monsters, his Health is now:" + Health "He fought" + counter "battles, and died.")
+    print("After fighting monsters, his Health is now:" + Health)
 
 
 
