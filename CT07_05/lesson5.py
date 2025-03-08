9429a8cba920a448d42bf5e11cc7ef49f548c2a5
Techical7 <chuayunjie.22@gmail.com>
2025-03-08 06:02:53 +0000

    Mar 8, 2025, 6:02 AM

--- CT07_05/lesson5.py
+++ CT07_05/lesson5.py
@@ -1,2 +1,66 @@
 print("Hello from lesson 5")
 
+# Task 1
+# import random
+
+# numbers = []
+
+# number = random.randint(1, 1000)
+
+# for i in range(100):
+#     number = random.randint(1, 1000)
+#     numbers.append(number)
+
+# Task 2
+# import random
+
+# random_numbers = []
+
+# while(len(random_numbers) <100):
+#     numbers=random.randint(1,1000)
+#     if (numbers not in random_numbers):
+#         random_numbers.append(numbers)
+
+# Task 3
+
+# print(max(random_numbers))
+# print(min(random_numbers))
+# print(sum(random_numbers)/len(random_numbers))
+
+# Task 4
+
+# namelist = ["Olivia", "Liam", "Emma", "Noah", "Ava", "Ethan",
+#             "Sophia", "Lucas", "Mia", "Aiden"
+#             ]
+# heightlist = [160, 165, 158, 170, 162, 168, 159, 172, 164, 166]
+
+
+# tallest = max(heightlist)
+# index_of_tallest=heightlist.index(tallest)
+# print(namelist[index_of_tallest])
+
+# shortest = min(heightlist)
+# index_of_shortest=heightlist.index(shortest)
+# print(namelist[index_of_shortest])
+
+# Task 5
+import random
+pokemons = [
+    "Pikachu", "Charizard", "Bulbasaur", "Squirtle",
+    "Jigglypuff", "Meowth", "Psyduck", "Eevee", "Snorlax",
+    "Mewtwo", "Lapras", "Gengar", "Dragonite", "Machamp",
+    "Arcanine", "Alakazam", "Gyarados", "Vaporeon", "Scyther",
+    "Electabuzz"
+]
+
+powers = [
+    55, 84, 49, 48, 45,
+    45, 52, 55, 110, 110,
+    85, 65, 134, 130, 110,
+    50, 125, 65, 110, 83
+]
+
+first_pokemon=random.choice(pokemons)
+first_pokemon_index = pokemons.index(first_pokemon)
+print(powers[first_pokemon_index])
+
