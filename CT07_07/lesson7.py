d1374d0357efe38c3e6525b3d07e5c472f3f1b49
Techical7 <chuayunjie.22@gmail.com>
2025-03-08 07:37:57 +0000

    Mar 8, 2025, 7:37 AM

--- CT07_07/lesson7.py
+++ CT07_07/lesson7.py
@@ -1,19 +1,20 @@
-# List
-import random
-counter = []
-Health = 100
-counter = str
-print("Hero starts on his adventure with Health: 100")
-# Random health lost
-while counter < 100:
-     random_number = random.randint(1,15)
-     Health -= random_number
-     counter+=1
-# End
-if Health < 1:
-    print("After fighting monsters, his Health is now:" + Health "He fought" + counter "battles, and died.")
-    print("After fighting monsters, his Health is now:" + Health)
+# # List
+# import random
+# counter = []
+# Health = 100
 
+# print("Hero starts on his adventure with Health: 100")
+# # Random health lost
+# while counter < 100:
+#      random_number = random.randint(1,15)
+#      Health -= random_number
+#      counter+=1
+
+# # End
+# if Health < 1:
+#     print("After fighting monsters, his Health is now:" + Health "He fought" + counter "battles, and died."):
+# else:
+#     print("After fighting monsters, his Health is now:" + Health)
 
 
 # List =[]
@@ -27,3 +28,7 @@ if Health < 1:
 # for food in List:
 #         print("You have ordered the following:")
 #         print(List)
+
+line = "ROBLOX,USD,0,813345,4.5,Games"
+words = line.split(",")
+print(words[2])
