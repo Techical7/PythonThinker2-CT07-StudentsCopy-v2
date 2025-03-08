29e8b8e597656e029d58b72678a1fb2a34fd994c
Techical7 <chuayunjie.22@gmail.com>
2025-03-08 06:24:11 +0000

    Mar 8, 2025, 6:24 AM

--- CT07_07/lesson7.py
+++ CT07_07/lesson7.py
@@ -3,6 +3,13 @@ counter = []
 Health = 100
 
 print("Hero starts on his adventure with Health: 100")
-random.randint(1,15)
+
+while counter < 100:
+     random_number = random.randint(1,15)
+     Health -= random_number
+     counter+=1
+
 if Health < 1:
-    print("After fighting monsters, his Health is now:" + Health )
+    print("After fighting monsters, his Health is now:")
+    print("After fighting monsters, his Health is now:" + Health)
+
