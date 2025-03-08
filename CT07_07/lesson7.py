34f909038af250d141daba9e0f228887083fd07d
Techical7 <chuayunjie.22@gmail.com>
2025-03-08 06:29:17 +0000

    Mar 8, 2025, 6:29 AM

--- CT07_07/lesson7.py
+++ CT07_07/lesson7.py
@@ -1,15 +1,30 @@
-import random
-counter = []
-Health = 100
+# import random
+# counter = []
+# Health = 100
 
-print("Hero starts on his adventure with Health: 100")
+# print("Hero starts on his adventure with Health: 100")
 
-while counter < 100:
-     random_number = random.randint(1,15)
-     Health -= random_number
-     counter+=1
+# while counter < 100:
+#      random_number = random.randint(1,15)
+#      Health -= random_number
+#      counter+=1
 
-if Health < 1:
-    print("After fighting monsters, his Health is now:")
-    print("After fighting monsters, his Health is now:" + Health)
+# if Health < 1:
+#     print("After fighting monsters, his Health is now:" + Health "He fought" + counter "battles, and died.")
+#     print("After fighting monsters, his Health is now:" + Health)
 
+
+#2
+List = []
+while True:
+    food = input("What would you like to order?")
+    if(food == "end"):
+        break
+    List.append(food)
+
+for food in List:
+    print(food)
+
+
+
+print("You have ordered the following:")
