c7b7458ef3d0e9aadec709f296ccd5cf838f4153
Techical7 <chuayunjie.22@gmail.com>
2025-03-16 08:18:42 +0000

    Mar 16, 2025, 8:18 AM

--- CT07_08/lesson8.py
+++ CT07_08/lesson8.py
@@ -5,7 +5,7 @@ list2 = [6, 5, 5]
 list3 = [9, 8, 7]
 
 total_list = sorted(set(list1+list2+list3))
-mid_point = len(total_list)//2
+mid_list = len(total_list)//2
 left = total_list[:mid_point]
 right = total_list[mid_point:]
 
