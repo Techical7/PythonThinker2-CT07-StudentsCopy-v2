eff6f09eeb9e22a80aaade9431b4609a76f37bf2
Techical7 <chuayunjie.22@gmail.com>
2025-03-16 08:52:20 +0000

    Mar 16, 2025, 8:52 AM

--- CT07_08/lesson8.py
+++ CT07_08/lesson8.py
@@ -66,9 +66,3 @@ for i in user_input:
     if i.islower() == True:
         has_lower = True
 
-
-    if is_8char_long and has_upper and has_lower and has_num is True:
-        print("Password is valid")
-else:
-    print("Invalid")
-
