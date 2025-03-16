4985a2bee4ae642058aea651398967f9046524d8
Techical7 <chuayunjie.22@gmail.com>
2025-03-16 08:24:58 +0000

    Mar 16, 2025, 8:24 AM

--- CT07_08/lesson8.py
+++ CT07_08/lesson8.py
@@ -1,14 +1,16 @@
-print("Hello from lesson 8")
+# print("Hello from lesson 8")
 
-list1 = [3, 2, 1]
-list2 = [6, 5, 5]
-list3 = [9, 8, 7]
+# list1 = [3, 2, 1]
+# list2 = [6, 5, 5]
+# list3 = [9, 8, 7]
 
-total_list = sorted(set(list1+list2+list3))
-mid_list = len(total_list)//2
-left = total_list[:mid_list]
-right = total_list[mid_list:]
+# total_list = sorted(set(list1+list2+list3))
+# mid_list = len(total_list)//2
+# left = total_list[:mid_list]
+# right = total_list[mid_list:]
 
-print(left)
-print(right)
+# print(left)
+# print(right)
+
+# task 1
 
