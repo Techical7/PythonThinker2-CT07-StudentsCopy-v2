9890553104d25584fef2888fa6a84777f584d04e
Techical7 <chuayunjie.22@gmail.com>
2025-03-22 06:57:42 +0000

    Mar 22, 2025, 6:57 AM

--- CT07_08/lesson8.py
+++ CT07_08/lesson8.py
@@ -156,8 +156,10 @@ sales_data=[
 sales_data == ()
 
 
-print("Top 3 selling Fruits:")
-Grapefruit,25,4.99, = 124.75
-Apple,50,1.99, = 99.50
-Orange,30,2.99, = 89.70
-
+print=[
+    ["Apple",50,1.99],# = 99.50
+    ["Banana",40,0.99],# = 39.60
+    ["Orange",30,2.99],# = 89.70
+    ["Grapefruit",25,4.99],# = 124.75
+    ["Mange",20,3.99]# = 79.80
+]
