d03dd0b0a03ccb173ea50e19099a14659fd27928
Techical7 <chuayunjie.22@gmail.com>
2025-03-22 06:14:31 +0000

    Mar 22, 2025, 6:14 AM

--- CT07_08/lesson8.py
+++ CT07_08/lesson8.py
@@ -96,15 +96,15 @@
 # reversed_text = ''.join(reversed_words) ## convert list to string
 # print(reversed_words)
 
-# Task 6
+# # Task 6
 
-word = "mom"
+# word = "mom"
 
-is_palidrome = word == word[::1]
-print(word[::1])
-print(is_palidrome)
+# is_palidrome = word == word[::1]
+# print(word[::1])
+# print(is_palidrome)
 
-while True:
-    sentence = input("Sentence: ")
+# while True:
+#     sentence = input("Sentence: ")
 
 
