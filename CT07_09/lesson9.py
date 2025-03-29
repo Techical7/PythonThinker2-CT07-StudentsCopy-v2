85c65406435d0299d38314892540b95fdcc79391
Techical7 <chuayunjie.22@gmail.com>
2025-03-29 06:20:07 +0000

    Mar 29, 2025, 6:20 AM

--- CT07_09/lesson9.py
+++ CT07_09/lesson9.py
@@ -1,7 +1,7 @@
 # print("Hello from lesson 9")
 isCorrect = False
 
-input("What has to be broken before you can use it? ")
+userInput = input("What has to be broken before you can use it? ")
 if input == "egg":
     isCorrect = True
 else:
