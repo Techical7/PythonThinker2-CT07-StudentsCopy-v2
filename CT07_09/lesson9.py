75c528d6d33de7272ac2e4c6c7cdcc4a88911fb1
Techical7 <chuayunjie.22@gmail.com>
2025-03-29 06:14:38 +0000

    Mar 29, 2025, 6:14 AM

--- CT07_09/lesson9.py
+++ CT07_09/lesson9.py
@@ -1,7 +1,9 @@
 # print("Hello from lesson 9")
 
-# input("What has to be broken before you can use it? ")
-# if input == "egg":
+input("What has to be broken before you can use it? ")
+if input == "egg":
+    isc
+
 isCorrect = False
 
 for i in range("egg"):
