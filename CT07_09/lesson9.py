de5589fd85f1292ee157a5d9056bfd9c38e7a8ee
Techical7 <chuayunjie.22@gmail.com>
2025-03-29 06:21:44 +0000

    Mar 29, 2025, 6:21 AM

--- CT07_09/lesson9.py
+++ CT07_09/lesson9.py
@@ -1,16 +1,16 @@
 # print("Hello from lesson 9")
-#Tas
-isCorrect = False
+# # Recap 1
+# isCorrect = False
 
-userInput = input("What has to be broken before you can use it? ")
-if userInput == "egg":
-    isCorrect = True
-else:
-    isCorrect = False
+# userInput = input("What has to be broken before you can use it? ")
+# if userInput == "egg":
+#     isCorrect = True
+# else:
+#     isCorrect = False
 
 
-if isCorrect == True:
-    print("Correct! Well done")
-else:
-    print("That's not correct. Try again! ")
+# if isCorrect == True:
+#     print("Correct! Well done")
+# else:
+#     print("That's not correct. Try again! ")
 
