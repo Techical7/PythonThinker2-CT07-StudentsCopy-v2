3d218a99704fb618038345466432f82c564def30
Techical7 <chuayunjie.22@gmail.com>
2025-03-29 06:33:05 +0000

    Mar 29, 2025, 6:33 AM

--- CT07_09/lesson9.py
+++ CT07_09/lesson9.py
@@ -1,32 +1,19 @@
 # print("Hello from lesson 9")
-# # Recap 1
-# isCorrect = False
+
 
 # userInput = input("What has to be broken before you can use it? ")
-# if userInput == "egg":
-#     isCorrect = True
+
+# isCorrect = False
+
+# lowercase_sentence = userInput.lower()
+# print(lowercase_sentence)
+# splitted_sentence = lowercase_sentence.split() #list
+# print(splitted_sentence)
+# for word in splitted_sentence:
+#     if word == "egg":
+#         isCorrect = True
+
+# if isCorrect:
+#     print("Correct! Well Done.")
 # else:
-#     isCorrect = False
-
-
-# if isCorrect == True:
-#     print("Correct! Well done")
-# else:
-#     print("That's not correct. Try again! ")
-
-userInput = input("What has to be broken before you can use it? ")
-
-isCorrect = False
-
-lowercase_sentence = userInput.lower()
-print(lowercase_sentence)
-splitted_sentence = lowercase_sentence.split() #list
-print(splitted_sentence)
-for word in splitted_sentence:
-    if word == "egg":
-        isCorrect = True
-
-if isCorrect:
-    print("Correct! Well Done.")
-else:
-    print("That is not correct! Try again")
+#     print("That is not correct! Try again")
