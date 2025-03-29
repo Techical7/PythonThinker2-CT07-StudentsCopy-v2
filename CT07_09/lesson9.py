ba9eb67504b49406ebd70cb32603d77f88872d52
Techical7 <chuayunjie.22@gmail.com>
2025-03-29 06:16:18 +0000

    Mar 29, 2025, 6:16 AM

--- CT07_09/lesson9.py
+++ CT07_09/lesson9.py
@@ -4,8 +4,7 @@ isCorrect = False
 input("What has to be broken before you can use it? ")
 if input == "egg":
     isCorrect = True
-
-for i in range():
-    isCorrect = True
 else:
+    isCorrect = False
+
     isCorrect = False
