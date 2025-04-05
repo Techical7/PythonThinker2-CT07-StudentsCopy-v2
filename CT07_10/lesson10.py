5dde57284da631e1e1b7aa17cfde308e22b8e9c7
Techical7 <chuayunjie.22@gmail.com>
2025-04-05 07:35:25 +0000

    Apr 5, 2025, 7:35 AM

--- CT07_10/lesson10.py
+++ CT07_10/lesson10.py
@@ -1,3 +1,38 @@
 # print("Hello from lesson 10")
 
-import 
+import turtle
+
+window = turtle.Screen()
+window.setup(600, 400)
+
+
+pen = turtle.Turtle()
+turtle.shape("turtle")
+turtle.fillcolor("orange")
+turtle.seth(0)
+turtle.pendown()
+# while True:
+#     for i in range(4):
+#         turtle.forward(100)
+#         turtle.left(90)
+
+# while True:
+#     for i in range(5):
+#         turtle.forward(100)
+#         turtle.left(72)
+
+    # window.mainloop()
+
+var1 = 200 # global
+
+def f():
+    #var1 = 500 #local variable
+    #global var2
+    var2 = 500
+    print(str(var2))
+def f2():
+    print(str(var1))
+
+
+f()
+print(str(var2))
