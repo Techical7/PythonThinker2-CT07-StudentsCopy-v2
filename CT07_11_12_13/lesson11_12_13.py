305e0b2e6ab0d2f3ac5aeb93a581461eabc9fc36
Techical7 <chuayunjie.22@gmail.com>
2025-04-12 06:13:13 +0000

    Apr 12, 2025, 6:13 AM

--- CT07_11_12_13/lesson11_12_13.py
+++ CT07_11_12_13/lesson11_12_13.py
@@ -1,11 +1,12 @@
 # print("Hello from lesson 11_12_13")
 
-var1 = 500
-def f():
-    global var1
-    var1 = 200
-    var2 = 300
-    print("var1 = " + str(var1))
+# var1 = 500
+# def f():
+#     global var1
+#     var1 = 200
+#     var2 = 300
+#     print("var1 = " + str(var1))
 
-f()
-print("var1 = " + str(var1))
+# f()
+# print("var1 = " + str(var1))
+
