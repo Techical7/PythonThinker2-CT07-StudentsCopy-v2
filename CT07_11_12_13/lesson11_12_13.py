fcb7b9dbb6f48d1d3da7a74299af1972ccb5ca87
Techical7 <chuayunjie.22@gmail.com>
2025-04-19 07:15:38 +0000

    Apr 19, 2025, 7:15 AM

--- CT07_11_12_13/lesson11_12_13.py
+++ CT07_11_12_13/lesson11_12_13.py
@@ -91,14 +91,8 @@ else:
     print("Please Key in your choice again as this spot is taken")
 
 def checkWin(board):
-     winning_condition [[board[0][0]], [board[0][1]], [board[0][2]],
-[board[1][0]], [board[1][1]], [board[1][2]],
-[board[3][0]], [board[3][1]], [board[3][2]],
-[board[0][0]], [board[1][0]], [board[2][0]],
-[board[0][1]], [board[1][1]], [board[2][1]],
-[board[0][2]], [board[1][2]], [board[2][2]],
-[board[0][2]], [board[1][1]], [board[2][0]],
-[board[2][0]], [board[1][1]], [board[0][2]],
+     winning_condition [[board[0][0]], [board[0][1]], [board[0][2]], [board[1][0]], [board[1][1]], [board[1][2]], [board[3][0]], [board[3][1]], [board[3][2]], [board[0][0]], [board[1][0]], [board[2][0]], [board[0][1]], [board[1][1]], [board[2][1]], [board[0][2]], [board[1][2]], [board[2][2]], [board[0][2]], [board[1][1]], [board[2][0]], [board[2][0]], [board[1][1]], [board[0][2]],]
+     for condition in WinningCondition
         if(condition[0] == condition[1] == condition[2]):
              if (condition!=" "):
             return True
