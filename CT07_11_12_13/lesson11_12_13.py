78db20c5c3f0245ca8ec81410f718b22a73f23d4
Techical7 <chuayunjie.22@gmail.com>
2025-04-19 07:12:43 +0000

    Apr 19, 2025, 7:12 AM

--- CT07_11_12_13/lesson11_12_13.py
+++ CT07_11_12_13/lesson11_12_13.py
@@ -93,4 +93,5 @@ else:
 def checkWin(board)
      winning_condition [[board[0][0]], [board[0][1]], [board[0][2]], [board[1][0]], [board[1][1]], [board[1][2]], [board[3][0]], [board[3][1]], [board[3][2]], [board[0][0]], [board[1][0]], [board[2][0]], [board[0][1]], [board[1][1]], [board[2][1]], [board[0][2]], [board[1][2]], [board[2][2]], [board[0][2]], [board[1][1]], [board[2][0]], [board[2][0]], [board[1][1]], [board[0][2]],]
      for condition in WinningCondition
-        if(condition[0] == condition[1])
+        if(condition[0] == condition[1] == condition[2]):
+             
