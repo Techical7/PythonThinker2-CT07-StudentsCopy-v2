1613f6af754aaf8ff2c3e89798272f47ff0464eb
Techical7 <chuayunjie.22@gmail.com>
2025-04-19 07:07:38 +0000

    Apr 19, 2025, 7:07 AM

--- CT07_11_12_13/lesson11_12_13.py
+++ CT07_11_12_13/lesson11_12_13.py
@@ -98,5 +98,5 @@ winning_condition = [
                     [board[0][1]], [board[1][1]], [board[2][1]],
                     [board[0][2]], [board[1][2]], [board[2][2]],
                     [board[0][2]], [board[1][1]], [board[2][0]],
-                    [board[2][0]], [board[1][1]], [board[0][2]],
+[board[2][0]], [board[1][1]], [board[0][2]],
 ]
