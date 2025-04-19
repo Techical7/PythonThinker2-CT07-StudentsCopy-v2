458b87ac396781c1dbce6d1435085fe11c17c054
Techical7 <chuayunjie.22@gmail.com>
2025-04-19 07:00:46 +0000

    Apr 19, 2025, 7:00 AM

--- CT07_11_12_13/lesson11_12_13.py
+++ CT07_11_12_13/lesson11_12_13.py
@@ -91,9 +91,9 @@ else:
     print("Please Key in your choice again as this spot is taken")
 
 winning_condition
-[board[0][0]], [board[0][1]], [board[0][2]]
-[board[][]], [board[][]], [board[][]]
-[board[][]], [board[][]], [board[][]]
-[board[][]], [board[][]], [board[][]]
-[board[][]], [board[][]], [board[][]]
+[board[0][0]], [board[0][1]], [board[0][2]],
+[board[1]0[]], [board[][]], [board[][]],
+[board[][]], [board[][]], [board[][]],
+[board[][]], [board[][]], [board[][]],
+[board[][]], [board[][]], [board[][]],
 [board[][]], [board[][]], [board[][]]
