95f1bfa87b5b94336723681e80f1bc0a342f47db
Techical7 <chuayunjie.22@gmail.com>
2025-04-26 06:38:54 +0000

    Apr 26, 2025, 6:38 AM

--- CT07_11_12_13/lesson11_12_13.py
+++ CT07_11_12_13/lesson11_12_13.py
@@ -102,17 +102,17 @@ def getPlayerMove(board):
             print("Spot taken, choose another one.")
 
 def checkWin(board):
-    
+
     winning_conditions = [
-        # Rows
+
         [board[0][0], board[0][1], board[0][2]],
         [board[1][0], board[1][1], board[1][2]],
         [board[2][0], board[2][1], board[2][2]],
-        # Columns
+
         [board[0][0], board[1][0], board[2][0]],
         [board[0][1], board[1][1], board[2][1]],
         [board[0][2], board[1][2], board[2][2]],
-        # Diagonals
+
         [board[0][0], board[1][1], board[2][2]],
         [board[0][2], board[1][1], board[2][0]],
     ]
