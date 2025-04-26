99958d482f1353a4a749439ecda1d445d5119511
Techical7 <chuayunjie.22@gmail.com>
2025-04-26 06:49:08 +0000

    Apr 26, 2025, 6:49 AM

--- CT07_11_12_13/lesson11_12_13.py
+++ CT07_11_12_13/lesson11_12_13.py
@@ -158,4 +158,5 @@ while True:
     # Hard: Implement the algorithm you have created for
 while True:
     level = input("Choose AI difficulty (1 = Easy, 2 = Medium, 3 = Hard): ")
-    if level in ['1', '2', '3']
+    if level in ['1', '2', '3']:
+        ai
